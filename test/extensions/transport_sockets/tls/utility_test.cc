59000988991206be60ec61367e97147b3149e7c8
Peter Leng <yleng@pinterest.com>
2023-12-18 15:12:02 -0800

    Update comments/description
    Signed-off-by: Peter Leng <yleng@pinterest.com>

--- test/extensions/transport_sockets/tls/utility_test.cc
+++ test/extensions/transport_sockets/tls/utility_test.cc
@@ -115,7 +115,7 @@ TEST(UtilityTest, TestGetSerialNumber) {
   EXPECT_EQ(TEST_SAN_DNS_CERT_SERIAL, Utility::getSerialNumberFromCertificate(*cert));
 }
 
-TEST(UtilityTest, TestSecondsUntilExpiratioWithExpiredCert) {
+TEST(UtilityTest, TestSecondsSinceEpochOfExpiratioWithExpiredCert) {
   bssl::UniquePtr<X509> cert = readCertFromFile(TestEnvironment::substitute(
       "{{ test_rundir }}/test/extensions/transport_sockets/tls/test_data/san_dns_cert.pem"));
   // Set a known date (2033-05-18 03:33:20 UTC) so that we get fixed output from this test.
@@ -126,7 +126,7 @@ TEST(UtilityTest, TestSecondsUntilExpiratioWithExpiredCert) {
   EXPECT_EQ(1724226690, Utility::getSecondsSinceEpoch(cert.get()).value());
 }
 
-TEST(UtilityTest, TestSecondsUntilExpiratioWithNotExpiredCert) {
+TEST(UtilityTest, TestSecondsSinceEpochOfWithNotExpiredCert) {
   bssl::UniquePtr<X509> cert = readCertFromFile(TestEnvironment::substitute(
       "{{ test_rundir }}/test/extensions/transport_sockets/tls/test_data/san_dns_cert.pem"));
   const time_t known_date_time = 0;
@@ -136,7 +136,7 @@ TEST(UtilityTest, TestSecondsUntilExpiratioWithNotExpiredCert) {
   EXPECT_EQ(1724226690, Utility::getSecondsSinceEpoch(cert.get()).value());
 }
 
-TEST(UtilityTest, TestSecondsUntilExpirationWithNullCert) {
+TEST(UtilityTest, TestSecondsSinceEpochOfWithNullCert) {
   Event::SimulatedTimeSystem time_source;
   EXPECT_EQ(absl::nullopt, Utility::getSecondsSinceEpoch(nullptr));
 }
